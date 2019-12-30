fc16de6c71cbd62717d5a4f3a1818ed5c1b388b7
sgtaylor16 <sgtaylor16@gmail.com>
2019-12-30 08:01:18 -0500

    Additional Functions to Write Meetings

--- parsenotes.py
+++ parsenotes.py
@@ -1,5 +1,8 @@
 import re
 from os import listdir
+from os.path import join
+from dateutil.parser import parse
+import numpy as np
 
 actiontest = re.compile("#[A-Z]")
 
@@ -39,14 +42,19 @@ def RenderLine(textline):
     else:
         return AddTag(textline,'p')
 
-def RenderLines(linelist):
+def RenderNotes(linelist):
     htmllist = []
     for line in linelist:
         htmllist.append(RenderLine(line))
     return htmllist
 
+def WriteNotes(noteslist):
+    totalstring = ''
+    for line in noteslist:
+        totalstring = totalstring + line + '\n'
+    return totalstring
+    
 def FindMeetings(folderpath):
-    allfiles = listdir(folderpath)
     mdfiles = [f for f in listdir(folderpath) if f[-3:] == ".md"]
     meetingslist1= []
     for file in mdfiles:
@@ -59,10 +67,19 @@ def FindMeetings(folderpath):
             meetingslist2.append(meeting)
     return meetingslist2
 
+def FindLatest(meetingname,folderpath):
+    allfiles = listdir(folderpath)
+    thismeeting = [file for file in allfiles if meetingname in file]
+    datearray = np.array([])
+    for onemeeting in thismeeting:
+        onedate = parse(re.match('[0-9]{1,2}-[0-9]{1,2}-[0-9]{1,4}',onemeeting)[0])
+        alldates = np.append(datearray,onedate)
+    maxindex = alldates.argmax()
+    latestmeeting = thismeeting[maxindex]
+    return join(folderpath,latestmeeting)
 
-    
-
-
-
-
+def ReadMeeting(meetingpath):
+    with open(meetingpath) as fh:
+        noteslist = RenderNotes(SplitbyCarriage(fh.read()))
+    return noteslist
     
